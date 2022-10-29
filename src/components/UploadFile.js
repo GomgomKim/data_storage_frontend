b9ca52dac224fe91e240b72abdf78bf6c3dee27a
GomgomKim <dhflwlsjfzz@naver.com>
2022-10-29 16:54:25 +0900

    [Init project] 1. Init data storage project (frontend)

---
+++ src/components/UploadFile.js
@@ -0,0 +1,65 @@
+import React, { useState, useRef } from 'react';
+import '../css/uploadFile.css';
+
+const UploadFile = () => {
+  const [dragActive, setDragActive] = useState(false);
+  const [fileName, setFileName] = useState("");
+  const inputRef = useRef(null);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      console.log("gomgom e.target.files", e.target.files);
+    }
+  };
+
+  // drag
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  };
+
+  // drop
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      setFileName(e.dataTransfer.files[0].name);
+      console.log(e.dataTransfer.files[0]);
+    }
+  };
+
+  const onButtonClick = () => {
+    inputRef.current.click();
+  };
+
+  return (
+    <>
+    <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
+      <input ref={inputRef} type="file" id="input-file-upload" multiple={true} onChange={handleChange} />
+      <label id="label-file-upload" htmlFor="input-file-upload" className={dragActive ? "drag-active" : "" }>
+        <div>
+          <p>Drag and drop your file here or</p>
+          <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
+        </div> 
+      </label>
+      { dragActive && 
+        <div id="drag-file-element" 
+          onDragEnter={handleDrag} 
+          onDragLeave={handleDrag} 
+          onDragOver={handleDrag} 
+          onDrop={handleDrop}>
+        </div> }
+      <span>{fileName}</span>
+    </form>
+    </>
+  );
+};
+
+export default UploadFile;
